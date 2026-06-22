9eff6135cc9ae615fe30fa11a8199042a0ee286a
htfab <mpw@htamas.net>
2026-06-22 22:24:25 +0200

    copy project files from ttgf0p2

--- test/test.py
+++ test/test.py
@@ -1,40 +1,43 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-FileCopyrightText: © 2025 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+from tqv import TinyQV
+
+# When submitting your design, change this to 16 + the peripheral number
+# in peripherals.v.  e.g. if your design is i_user_simple00, set this to 16.
+# The peripheral number is not used by the test harness.
+PERIPHERAL_NUM = 16
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    # Set the clock period to 100 ns (10 MHz)
+    clock = Clock(dut.clk, 100, unit="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    # Interact with your design's registers through this TinyQV class.
+    # This will allow the same test to be run when your design is integrated
+    # with TinyQV - the implementation of this class will be replaces with a
+    # different version that uses Risc-V instructions instead of the SPI 
+    # interface to read and write the registers.
+    tqv = TinyQV(dut, PERIPHERAL_NUM)
+
+    # Reset, always start the test by resetting TinyQV
+    await tqv.reset()
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Test register write and read back
+    await tqv.write_reg(0, 208)
+    assert await tqv.read_reg(0) == 208
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    for i in range(16):
+        val = (0x165ca97fe3 >> i) & 0xff
+        await tqv.write_reg(i, val)
+        assert await tqv.read_reg(i) == val
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
